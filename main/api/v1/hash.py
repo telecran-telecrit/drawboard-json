6afacc8fc92e6eef17bb969590968662be9ea900
Lipis <lipiridis@gmail.com>
2020-01-17 20:18:16 +0200

    Docs and more (#1)

--- main/api/v1/hash.py
+++ main/api/v1/hash.py
@@ -19,14 +19,18 @@ parser.add_argument('json')
 
 @api_v1.resource('/<string:drawing_hash>/', endpoint='api.create')
 class DrawingCreateAPI(flask_restful.Resource):
+  def get(self, drawing_hash):
+    drawing_db = model.Drawing.get_by('hash', drawing_hash)
+    if not drawing_db:
+      helpers.make_not_found_exception('Drawing %s not found' % drawing_hash)
+    return flask.redirect(flask.url_for('api.hash', drawing_hash=drawing_hash))
+
   def post(self, drawing_hash):
     drawing_db = model.Drawing.get_by('hash', drawing_hash)
     if drawing_db:
-      return flask.jsonify(drawing_db.json)
+      return flask.redirect(flask.url_for('api.hash', drawing_hash=drawing_hash))
     try:
       drawing_json = json.loads(flask.request.data)
-      import logging
-      logging.info('########################## %r' % flask.request.data)
       m = hashlib.md5()
       m.update(str(drawing_json))
       if m.hexdigest() != drawing_hash:
@@ -38,7 +42,6 @@ class DrawingCreateAPI(flask_restful.Resource):
       helpers.make_not_found_exception('Not valid JSON')
 
     return flask.jsonify(drawing_db.json)
-
 
 
 @api_v1.resource('/<string:drawing_hash>.json', endpoint='api.hash')
