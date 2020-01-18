d3d371bba79b42445383a97a58762f547c6ed080
Lipis <lipiridis@gmail.com>
2020-01-18 03:40:49 +0200

    Simpler (#2)

--- main/api/v1/hash.py
+++ main/api/v1/hash.py
@@ -17,31 +17,25 @@ parser = reqparse.RequestParser()
 parser.add_argument('json')
 
 
-@api_v1.resource('/<string:drawing_hash>/', endpoint='api.create')
+@api_v1.resource('/post/', endpoint='api.create')
 class DrawingCreateAPI(flask_restful.Resource):
-  def get(self, drawing_hash):
-    drawing_db = model.Drawing.get_by('hash', drawing_hash)
-    if not drawing_db:
-      helpers.make_not_found_exception('Drawing %s not found' % drawing_hash)
-    return flask.redirect(flask.url_for('api.hash', drawing_hash=drawing_hash))
-
-  def post(self, drawing_hash):
-    drawing_db = model.Drawing.get_by('hash', drawing_hash)
-    if drawing_db:
-      return flask.redirect(flask.url_for('api.hash', drawing_hash=drawing_hash))
+  def post(self):
     try:
       drawing_json = json.loads(flask.request.data)
       m = hashlib.md5()
       m.update(str(drawing_json))
-      if m.hexdigest() != drawing_hash:
-        helpers.make_not_found_exception('Not a valid hash for that JSON (%s)' % m.hexdigest())
-      else:
+      drawing_hash = m.hexdigest()
+      drawing_db = model.Drawing.get_by('hash', drawing_hash)
+      if not drawing_db:
         drawing_db = model.Drawing(hash=drawing_hash, json=drawing_json)
-        drawing_db.put()
+      drawing_db.put()
     except (ValueError, AssertionError):
       helpers.make_not_found_exception('Not valid JSON')
 
-    return flask.jsonify(drawing_db.json)
+    return flask.jsonify({
+      'hash': drawing_hash,
+      'json': flask.url_for('api.hash', drawing_hash=drawing_hash, _external=True),
+    })
 
 
 @api_v1.resource('/<string:drawing_hash>.json', endpoint='api.hash')
